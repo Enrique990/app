87ab979eee15f48dc5871719547bfffaa0ec78b4
Enrique990 <enrique.taleno16055@est.uca.edu.ni>
2023-05-01 16:49:26 -0600

    exe

--- app.cpp
+++ app.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<conio.h>
 using namespace std;
 
 #include <iostream>
@@ -234,5 +235,6 @@ int main() {
                 cout << "Invalid option. Try again." << endl;
         }
     }
+    getch();
     return 0;
 }
